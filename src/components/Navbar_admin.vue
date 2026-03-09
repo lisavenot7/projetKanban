726966646ab1cf1d4225a940a9cba5fb809eda83
lisavenot7 <venot.lisa.lv@gmail.com>
2026-03-09 16:18:16 +0100

    barre admin

--- src/components/Navbar_admin.vue
+++ src/components/Navbar_admin.vue
@@ -7,8 +7,10 @@
   <nav class="navbar">
     <div class="logo">Planora</div>
     <div class="links">
-       <a><router-link to="/">Accueil</router-link></a>
+       <a><router-link to="/admin">Accueil</router-link></a>
       <a><router-link to="/connexion">Connexion</router-link></a>
+      <a><router-link to="/connexion">Connexion</router-link></a>
+      <a><router-link to="/">Deconnexion</router-link></a>
     </div>
   </nav>
 </template>
